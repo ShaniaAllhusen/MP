cc49ab3b94426ba5de84bf58bacfed7f949996d4
Dominik212 <sdominik011@yahoo.de>
2016-11-19 11:33:33 +0100

    Tabellenklassen fertig gemacht

---
+++ src/tabellenklassen/Benutzer.java
@@ -0,0 +1,27 @@
+package tabellenklassen;
+
+public class Benutzer {
+
+	private int id;
+	private String username;
+	private String passowrt;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassowrt() {
+		return passowrt;
+	}
+	public void setPassowrt(String passowrt) {
+		this.passowrt = passowrt;
+	}
+}
---
+++ src/tabellenklassen/Mitglied.java
@@ -0,0 +1,63 @@
+package tabellenklassen;
+
+public class Mitglied {
+
+	private int id;
+	private String vorname;
+	private String nachname;
+	private String geburtsdatum;
+	private String strasse;
+	private String plz;
+	private String ort;
+	private int benutzer_id;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getVorname() {
+		return vorname;
+	}
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+	public String getNachname() {
+		return nachname;
+	}
+	public void setNachname(String nachname) {
+		this.nachname = nachname;
+	}
+	public String getGeburtsdatum() {
+		return geburtsdatum;
+	}
+	public void setGeburtsdatum(String geburtsdatum) {
+		this.geburtsdatum = geburtsdatum;
+	}
+	public String getStrasse() {
+		return strasse;
+	}
+	public void setStrasse(String strasse) {
+		this.strasse = strasse;
+	}
+	public String getPlz() {
+		return plz;
+	}
+	public void setPlz(String plz) {
+		this.plz = plz;
+	}
+	public String getOrt() {
+		return ort;
+	}
+	public void setOrt(String ort) {
+		this.ort = ort;
+	}
+	public int getBenutzer_id() {
+		return benutzer_id;
+	}
+	public void setBenutzer_id(int benutzer_id) {
+		this.benutzer_id = benutzer_id;
+	}
+	
+}

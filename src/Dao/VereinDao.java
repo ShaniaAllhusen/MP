62ecd6ccb64af61737643dd60bd6cc2a5460d563
Fenja Ehlers <fenja-ehlers@hotmail.de>
2017-01-12 17:41:04 +0100

    neue Select Methode

--- src/Dao/VereinDao.java
+++ src/Dao/VereinDao.java
@@ -104,5 +104,40 @@ public class VereinDao {
 		}
 		return arrayListTraining; 
 	}
+	
+	
+	public ArrayList<Training> select (String halleId, String tag) {
+		ArrayList<Training> arrayListTraining = new ArrayList<Training>(); 
+		Connection conn = null;
+		PreparedStatement preparedStatement = null;
+		try {
+			conn = DriverManager.getConnection(CONNECTIONSTRING + datei); 
+			String sql = "SELECT t.id, t.halle_id, m.id, m.name , s.id , s.name, h.id, h.name, h.strasse, h.plz, h.ort FROM training t "
+					+ "JOIN mannschaft m ON t.mannschaft_id = m.id "
+					+ "JOIN sportart s ON m.sportart_id = s.id "
+					+ "JOIN halle h ON t.halle_id = h.id"
+					+ "WHERE h.id = ? AND ";
+			preparedStatement = conn.prepareStatement(sql);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			while (resultSet.next()) {
+				Sportart sportart = new Sportart(resultSet.getInt("s.id"),resultSet.getString("s.name"));
+				Mannschaft mannschaft = new Mannschaft(resultSet.getInt("m.id"), resultSet.getString("m.name"), sportart);
+				Halle halle = new Halle(resultSet.getInt("h.id"), resultSet.getString("h.name"), resultSet.getString("h.strasse"), 
+						resultSet.getString("h.plz"), resultSet.getString("h.ort"));
+				Training training = new Training(resultSet.getInt("t.id"), mannschaft, halle);
+				arrayListTraining.add(training); 
+			} 
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				preparedStatement.close();
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return arrayListTraining; 
+	}
 }
 
